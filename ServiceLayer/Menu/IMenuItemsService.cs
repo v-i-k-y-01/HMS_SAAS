469a115fc413ab3b7c4b598d82c6e6fcacce5dcc
viky s <vikycosteta@gmail.com>
2025-06-28 18:10:59 +0530

    get by id nad scripts are updated

--- ServiceLayer/Menu/IMenuItemsService.cs
+++ ServiceLayer/Menu/IMenuItemsService.cs
@@ -5,4 +5,5 @@ namespace HMS_SAAS.ServiceLayer.Menu;
 public interface IMenuItemsService
 {
     Task<List<MenuItems>> GetMenuItemsAsync();
+    Task<MenuItems> GetMenuItemByIdAsync(string itemId);
 }
