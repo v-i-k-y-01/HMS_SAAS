ebbd30939a39cd0b6a74401795c3b9ee8b803968
Vinush <vinush.sadayappan@tetruscorp.com>
2025-07-02 14:19:03 -0400

    Order Module Create, Get, Update

--- Controllers/Menu/MenuItemController.cs
+++ Controllers/Menu/MenuItemController.cs
@@ -62,4 +62,3 @@ public class MenuItemController(IMenuItemsService menuItemsService, ILogger<Menu
 
     }
 }
-}
---
+++ DataLayer/Models/OrdersResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace HMS_SAAS.DataLayer.Models
+{
+    public class OrdersResponse
+    {
+        public int OrderId { get; set; }
+        public string ItemId { get; set; }
+        public string ItemName {  get; set; }
+        public int Quantity {  get; set; }
+        public DateTime? CreatedAt {  get; set; }
+        public DateTime? UpdatedAt { get; set; } = DateTime.Now;
+    }
+    public class OrdersRequest
+    {
+        public string ItemId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
